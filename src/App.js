9380936c2c057e2879a0558b62c12244d53ab2ee
Yurii-maker <yuri19961204@gmail.com>
2022-08-20 06:44:18 +0300

    change url

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     return (
         <div className="App">
            <Routes>
-               <Route path={'/'} element={<HomePage/>}>
+               <Route path={'/chuck_norris_chat'} element={<HomePage/>}>
                    <Route path={':id'} element={<Chat/>}/>
                </Route>
            </Routes>

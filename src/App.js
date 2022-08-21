d10bddb1da89bf8269ecff5b8e87b0ad95e632b7
Yurii-maker <yuri19961204@gmail.com>
2022-08-21 17:59:02 +0300

    add notFoundPage

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path='/chuck_norris_chat' element={<HomePage/>}>
+                <Route path={'chuck_norris_chat'} element={<HomePage/>}>
                     <Route path={':id'} element={<Chat/>}/>
-                    <Route path={'*'} element={<NotFoundPage/>}/>
                 </Route>
+                <Route path={'*'} element={<NotFoundPage/>}/>
             </Routes>
         </div>
     );
